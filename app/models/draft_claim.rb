a91ba22c799e54e53efaf2a9cad62fa2769229d4
Jeffrey Horn <jeff@jrhorn.me>
2014-07-17 18:39:24 -0400

    Add draft claim

---
+++ app/models/draft_claim.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: claims
+#
+#  id           :integer          not null, primary key
+#  number       :string(255)
+#  filed_on     :date
+#  amount_cents :integer
+#  policy_id    :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class DraftClaim < Claim
+  establish_connection("draft_#{Rails.env}")
+end
