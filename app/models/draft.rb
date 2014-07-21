f0d5ee4782e45be92fccbc905b0681614a21bc7c
Phil McMillan <phil.mcmillan@ent0.com>
2014-07-21 16:16:12 -0400

    added initial version of multis submit process

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -25,10 +25,33 @@ module Draft
 
   def self.publish(draft_claim)
     currently_published = Claim.find(draft_claim.id)
+    currently_published.update_attributes(diff_obj(draft_claim, currently_published)))
+
+    published_claimants = currently_published.claimants
+    draft_claimants = draft_claim.claimants
+    publish_multis(draft_claimants, published_claimants, Claimant)
+
+  end
+
+  def self.publish_multis(drafts, published, multi_type)
+    published.select { |c| drafts.none? { |d| d.id ==  c.id}}.each { |dc| dc.delete}
+    drafts.each do |draft|
+      if(draft.id < 0)
+        new_multi = draft.becomes(mult_type)
+        new_multi.id = nil
+        new_multi.instance_variable_set(@'new_record', true)
+        new_multi.save
+      else
+        published = published.select {|p| p.id = draft.id}
+        published.update_attributes(diff_obj(draft, published))
+      end
+    end
+  end
+
+  def self.diff_obj(draft, published)
     pub_diff = draft_claim.diff(currently_published).inject({}) do |h, (key, value)|
       h[key] =  value[0]
       h
     end
-    currently_published.update_attributes(pub_diff)
   end
 end
