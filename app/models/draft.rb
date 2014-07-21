9790fa474682553dfca025da795a12133eda0075
Jeffrey Horn <jeff@jrhorn.me>
2014-07-21 12:21:36 -0400

    JRH - K471 - Clean up draft creation using new API

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -4,46 +4,23 @@ class Draft
   #
   # At bootup, store a list of objects that are draftable.
   # When we make a draft, draft all objects at the same time.
-  #
-  # ...
-  class << self
-    def models
-      Dir['app/models/*.rb'].map {|f| File.basename(f, '.*').camelize.constantize.name }
-    end
 
-    # these must be activerecord models, no way around it
-    def draftables
-      models.select { |m| m.constantize.draftable? unless m.match 'Draft' }
-    end
+  # def initialize(base_instance)
+  #   self.class.create_drafts(base_instance)
+  # end
+
+  # def self.create_drafts(base_instance)
+  #   base_instance.create_draft
+  #   DRAFTABLES.each do |d|
+  #     base_instance.send d.downcase
+  #   end
+  # end
+
+  def initialize(claim)
+    draft_claim = claim.create_draft
+    claim.policy.create_draft
+    claim.claim_submissions.each { |cs| cs.create_draft }
+    claim.claimants.each { |c| c.create_draft }
+    draft_claim
   end
-
-
-
-  # def self.make(claim)
-  #   draft_claim = DraftClaim.draft(claim)
-  #   draft_claim.policy = DraftPolicy.draft(claim.policy)
-  #   claim.claim_submissions.each_with_index do |claim_submission, index|
-  #     draft_claim.claim_submissions[index] = DraftClaimSubmission.draft(claim_submission)
-  #   end
-  #   claim.claimants.each_with_index do |claimant, index|
-  #     draft_claim.claimants[index] = DraftClaimant.draft(claimant)
-  #   end
-  #   draft_claim
-  # end
-
-  # def self.make_savable(draft_claim)
-  #   draft_claim.instance_variable_set("@new_record",true)
-  #   draft_claim.policy.instance_variable_set("@new_record", true)
-  #   draft_claim.claim_submissions.each { |cs| cs.instance_variable_set("@new_record", true) }
-  #   draft_claim.claimants.each { |c| c.instance_variable_set("@new_record", true) }
-  #   draft_claim
-  # end
-
-  # def self.save_all(draft_claim)
-  #   draft_claim.claimants.each { |c| c.save }
-  #   draft_claim.claim_submissions.each { |c| c.save }
-  #   draft_claim.policy.save
-  #   draft_claim.save
-  # end
-
 end
