b812b82e18a1e7be0aedd4716c479a323ad06e01
Phil McMillan <phil.mcmillan@ent0.com>
2014-07-21 17:42:12 -0400

    added further publish logic for multis. Configured draft claim to intialize Id negative unless existing Id.

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -19,37 +19,55 @@ module Draft
     claim.policy.create_draft
     claim.claim_submissions.each { |cs| cs.create_draft }
     claim.claimants.each { |c| c.create_draft }
+    draft_claim.id = DraftClaim.minimum('id') - 1 unless draft_claim.id > 0
     draft_claim.save
     draft_claim
   end
 
   def self.publish(draft_claim)
-    currently_published = Claim.find(draft_claim.id)
-    currently_published.update_attributes(diff_obj(draft_claim, currently_published)))
+    Claim.transaction do
 
-    published_claimants = currently_published.claimants
-    draft_claimants = draft_claim.claimants
-    publish_multis(draft_claimants, published_claimants, Claimant)
+      if draft_claim.id > 0
+        published_claim = Claim.find(draft_claim.id)
+        published_claim.update_attributes(diff_obj(draft_claim, published_claim))
+      else
+        published_claim = draft_claim.becomes(Claim)
+        published_claim.id = nil
+        published_claim.instance_variable_set('@new_record', true)
+        published_claim.save
+      end
 
+      published_claimants = published_claim.claimants
+      draft_claimants = draft_claim.claimants
+      publish_multis(draft_claimants, published_claimants, Claimant)
+
+      published_policy = published_claim.policy
+      draft_policy = draft_claim.policy
+      published_policy.update_attributes(diff_obj(draft_policy, published_policy))
+
+      draft_policy.delete
+      draft_claimants.delete_all
+      draft_claim.delete
+    end
   end
 
   def self.publish_multis(drafts, published, multi_type)
-    published.select { |c| drafts.none? { |d| d.id ==  c.id}}.each { |dc| dc.delete}
+    published.select { |p| drafts.none? { |d| d.id ==  p.id}}.each { |delpub| delpub.delete}
     drafts.each do |draft|
       if(draft.id < 0)
-        new_multi = draft.becomes(mult_type)
+        new_multi = draft.becomes(multi_type)
         new_multi.id = nil
-        new_multi.instance_variable_set(@'new_record', true)
+        new_multi.instance_variable_set('@new_record', true)
         new_multi.save
       else
-        published = published.select {|p| p.id = draft.id}
+        published = published.select {|p| p.id == draft.id}
         published.update_attributes(diff_obj(draft, published))
       end
     end
   end
 
   def self.diff_obj(draft, published)
-    pub_diff = draft_claim.diff(currently_published).inject({}) do |h, (key, value)|
+    pub_diff = draft_claim.diff(published_claim).inject({}) do |h, (key, value)|
       h[key] =  value[0]
       h
     end
